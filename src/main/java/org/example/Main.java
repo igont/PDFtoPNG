47b5ec4a002e679b18a5f468736a73d25fb22358
Игорь Гонтаренко <igorgontaren@mail.ru>
2023-03-12 22:02:55 +0300

    Works with PDFBox 2x version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,91 @@
 package org.example;
 
-import com.aspose.pdf.Document;
-import com.aspose.pdf.devices.PngDevice;
-import com.aspose.pdf.devices.Resolution;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Main
 {
-
-	static String src = "C:\\Users\\Igor\\Desktop\\СГП Гонтаренко.pdf";
-	static String dest = "C:\\Users\\Igor\\Desktop\\СГП Гонтаренко.png";
-
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
 	{
-		Document pdfDocument = new Document(src);
 
-		// Loop through all the pages of PDF file
-		for(int pageCount = 1; pageCount <= pdfDocument.getPages().size(); pageCount++)
+		long currentTimeMillis = System.currentTimeMillis();
+
+		File dir = new File(".");
+		File[] filesList = dir.listFiles();
+		if(filesList != null)
 		{
-			// Create stream object to save the output image
-			try(java.io.OutputStream imageStream = new java.io.FileOutputStream(src.replace(".pdf","_" + pageCount + ".png")))
+			for(File file : filesList)
 			{
-				// Create Resolution object
-				Resolution resolution = new Resolution(100000);
-				// Create PngDevice object with particular resolution
-				PngDevice pngDevice = new PngDevice(resolution);
-				// Convert a particular page and save the image to stream
-				pngDevice.process(pdfDocument.getPages().get_Item(pageCount), imageStream);
+				if(file.isFile() && getFileExtension(file.getName()).equalsIgnoreCase("pdf"))
+				{
+					convert(file);
+				}
 			}
+		}
+
+		long diff = System.currentTimeMillis() - currentTimeMillis;
+
+		System.out.print(diff);
+
+	}
+
+	private static String getFileExtension(String fileName)
+	{
+		if(fileName == null || fileName.equals("")) return "undefined";
+		int dotIndex = fileName.lastIndexOf(".");
+		return (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
+	}
+
+	private static void convert(File sourceFile)
+	{
+		try
+		{
+			String destinationDir = sourceFile.getName().replace(".pdf", "") + "/"; // converted images from pdf document are saved here
+
+			File destinationFile = new File(destinationDir);
+			if(!destinationFile.exists())
+			{
+				boolean fileCreated = destinationFile.mkdir();
+				if(fileCreated) System.out.println("Folder Created -> " + destinationFile.getAbsolutePath());
+			}
+			if(sourceFile.exists())
+			{
+				System.out.println("Images copied to Folder: " + destinationFile.getName());
+
+				PDDocument document = PDDocument.load(sourceFile);
+				PDPageTree pdPageTree = document.getDocumentCatalog().getPages();
+				System.out.println("Total files to be converted -> " + pdPageTree.getCount());
+
+				PDFRenderer pdfRenderer = new PDFRenderer(document);
+
+				String fileName = sourceFile.getName().replace(".pdf", "");
+				int pageNumber = 1;
+				for(int page = 0; page < document.getNumberOfPages(); ++page)
+				{
+
+					BufferedImage image = pdfRenderer.renderImageWithDPI(page, 150, ImageType.RGB);
+					File outputFile = new File(destinationDir + fileName + "_" + pageNumber + ".jpg");
+					System.out.println("Image Created -> " + outputFile.getName());
+					ImageIO.write(image, "jpg", outputFile);
+					pageNumber++;
+				}
+				document.close();
+				System.out.println("Converted Images are saved at -> " + destinationFile.getAbsolutePath());
+			} else
+			{
+				System.err.println(sourceFile.getName() + " File not exists");
+			}
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
